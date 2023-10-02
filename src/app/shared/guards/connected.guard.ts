ea4c184f983f5106f4430477b23b45ffe734fb49
AntoTilm <antoine.tilman@gmail.com>
2023-10-02 21:13:32 +0200

    Partie Guards full commentary

--- src/app/shared/guards/connected.guard.ts
+++ src/app/shared/guards/connected.guard.ts
@@ -4,24 +4,21 @@ import { FakeAuthService } from '../services/fake-auth.service';
 import { map } from 'rxjs';
 
 export const connectedGuard: CanActivateFn = (route, state) => {
-  //Comme la guard est une fonction et plus une classe depuis la dernière maj, on doit injecter les services et les modules, comme suit : inject(Router)
-  // Antoine: injecter le Router dans notre classe permet d'utiliser les redirections (en cas de return false par exemple). Un simple if/else return true/false permet d'avoir déja une guard 
+  // Comme la guard est une fonction et plus une classe depuis la dernière maj, on doit injecter les services et les modules, comme suit : const router = inject(Router)
+  // Antoine: injecter le Router (car ce n'est pas une classe et qu'il n'y a donc pas de constructeur) permet d'utiliser les redirections (en cas de return false par exemple). 
+  // Antoine: Un simple if/else return true/false permet d'avoir déja une guard 
+  // Antoine: Injecter le router permet, une fois nommé (ici const router) d'appeler les méthodes de Router comme router.navigateByUrl 
   const router = inject(Router); 
   const fakeAuthService = inject(FakeAuthService);
 
-  // On vérifie si l'utilisateur est connecté via son id stocké dans le localStorage
-  // let userId : string | null = localStorage.getItem('userId');
-  // if(!userId) {
-  //   //Si pas connecté, on redirige vers la page de connexion
-  //   router.navigateByUrl('/demo/demo5');
-  //   return false;
-  // }
+  // Note1
 
-  //Plus safe : Plutôt que se baser sur le localStorage, qui n'est pas totalement fiable, on va plutôt surveiller l'observable connectedUser présent dans notre service :
+  // Plus safe : Plutôt que se baser sur le localStorage, qui n'est pas totalement fiable, on va plutôt surveiller l'observable connectedUser présent dans notre service. 
+  // Antoine: Pour ce faire il faut l'injecter comme on a fait pour le router: const fakeAuthService = inject(FakeAuthService) puis aller chercher l'obervable $connectedUser du service
   return fakeAuthService.$connectedUser.pipe(map((res) => {
-    //l'opérateur map nous permettra de savoir ce qu'il y a à dans l'observable connectedUser à l'instant où la Guard va être appelée
-    //Pas besoin de s'abonner à l'observable, la guard n'est active que le temps d'accéder à la route
-    //res contient soit un User, soit undefined
+    // Antoine: L'observable $connectedUser du service FakeAuthService émettra soit un objet User (si l'utilisateur est connecté) soit undefined (si l'utilisateur n'est pas connecté). 
+    // Antoine: Le pipe map provenant de l'opérateur rxjs est utilisé pour transformer ces émissions en true si l'utilisateur est connecté (et donc peut accéder à la route) et en false s'il n'est pas connecté (et doit être redirigé).
+    // Antoine: res est le paramètre utilisé pour représenter chaque émission (valeur) émise par l'Observable $connectedUser
     if(res) {
       return true;
     } 
@@ -30,7 +27,14 @@ export const connectedGuard: CanActivateFn = (route, state) => {
       return false;
     }
   } ))
-
-  //La guard renvoie un boolean (true si j'ai droit d'accéder au Composant, false si pas)
-  //return true;
 };
+
+  // Note1
+  // Ancienne méthode, à éviter
+  // On vérifie si l'utilisateur est connecté via son id stocké dans le localStorage
+  // let userId : string | null = localStorage.getItem('userId');
+  // if(!userId) {
+  //   //Si pas connecté, on redirige vers la page de connexion
+  //   router.navigateByUrl('/demo/demo5');
+  //   return false;
+  // }
